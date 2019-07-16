1f8835de52463f536b5d882ca63085fc7603390c
Johannes Debler <johannes.debler@gmail.com>
2019-07-16 10:19:55 +0800

    edited help messages

--- interproscan_parse.py
+++ interproscan_parse.py
@@ -7,8 +7,8 @@ from pathlib import Path
 
 #Parse commandline arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--proteins', help='path to a fasta file containing protein sequences, (default = "proteins.fasta" file in script directory)')
-parser.add_argument('-i', '--interproscan', help='path to an interproscan.out file (default = "interproscan.tsv" file in script directory)')
+parser.add_argument('-p', '--proteins', help='path to a fasta file containing protein sequences, (default = "*.proteins.fasta" file in script directory)')
+parser.add_argument('-i', '--interproscan', help='path to an interproscan.out file (default = "*.interproscan.tsv" file in script directory)')
 parser.add_argument('-o', '--output', help='path and name of output file (default = searchterm.fasta)')
 parser.add_argument('-s', '--searchterm', help='a string of what you are looking for (default = amylase)')
 parser.add_argument('-d', '--database', help='limit hits to certain database (pfam, tmhmm, panther, prosite, mobidb, signalp, prints, prodom)')
